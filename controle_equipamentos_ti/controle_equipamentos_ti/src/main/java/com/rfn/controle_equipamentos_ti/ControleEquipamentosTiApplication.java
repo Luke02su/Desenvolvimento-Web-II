76fbbd399336d236df78f7e52c82ebb38a65704f
Lucas <68867449+Luke02su@users.noreply.github.com>
2025-05-23 22:05:13 -0300

    Add files via upload

---
+++ controle_equipamentos_ti/controle_equipamentos_ti/src/main/java/com/rfn/controle_equipamentos_ti/ControleEquipamentosTiApplication.java
@@ -0,0 +1,13 @@
+package com.rfn.controle_equipamentos_ti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEquipamentosTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEquipamentosTiApplication.class, args);
+	}
+
+}
---
+++ controle_equipamentos_ti/controle_equipamentos_ti/src/test/java/com/rfn/controle_equipamentos_ti/ControleEquipamentosTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfn.controle_equipamentos_ti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEquipamentosTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
