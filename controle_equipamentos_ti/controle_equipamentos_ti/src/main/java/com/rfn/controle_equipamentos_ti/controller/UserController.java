4f4daf2d1a0d2d52aa92d9cb5fe9da5ae4a0dec3
Lucas <68867449+Luke02su@users.noreply.github.com>
2025-07-09 08:15:12 -0300

    userController ok

---
+++ controle_equipamentos_ti/controle_equipamentos_ti/src/main/java/com/rfn/controle_equipamentos_ti/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.rfn.controle_equipamentos_ti.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.rfn.controle_equipamentos_ti.model.User;
+import com.rfn.controle_equipamentos_ti.service.UserService;
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // Go to Registration Page
+    @GetMapping("/register")
+    public String register() {
+        return "user/registerUser";
+    }
+
+    // Read Form data to save into DB
+    @PostMapping("/saveUser")
+    public String saveUser(
+            @ModelAttribute User user,
+            Model model) {
+        Integer id = userService.saveUser(user);
+        String message = "User '" + id + "' saved successfully !";
+        model.addAttribute("msg", message);
+        return "user/registerUser";
+    }
+
+    @GetMapping("/accessDenied")
+	public String getAccessDeniedPage() {
+		return "user/accessDeniedPage";
+	}
+}
