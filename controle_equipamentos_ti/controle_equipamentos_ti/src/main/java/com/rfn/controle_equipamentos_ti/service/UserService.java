da570aa7a85de19add222a72c30516c51e610a2c
Lucas <68867449+Luke02su@users.noreply.github.com>
2025-07-09 08:17:43 -0300

    userService ok

---
+++ controle_equipamentos_ti/controle_equipamentos_ti/src/main/java/com/rfn/controle_equipamentos_ti/service/UserService.java
@@ -0,0 +1,8 @@
+package com.rfn.controle_equipamentos_ti.service;
+
+import com.rfn.controle_equipamentos_ti.model.User;
+
+public interface UserService {
+	
+	public Integer saveUser(User user);
+}
